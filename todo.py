5d5204bc2cedcae96f48ae1635e8bcaa5b067511
Cherry342 <aokagejasmin@brooklynsteamcenter.org>
2023-03-01 10:53:21 -0500

    add todo.py

--- todo.py
+++ todo.py
@@ -1,24 +1,31 @@
+import os
 from flask import Flask, render_template, request
 app= Flask(__name__)
 
-my_todos=[
+my_todos=[ 
     'Game',
     'Review'
+
 ]
 @app.route("/")
-
-def index():
+def index(my_todos):
     return render_template(
         "todo.html.jinja", 
-        todos= my_todos 
-     my_variable="reviewing"
-     my_list=["review", "kahoot"]
-     )
-@app.route("/add", methods=['POST'])
-def add(new_todo):
+        todos= my_todos)
+
+
+
+@app.route("/add", methods=['POST', 'GET'])
+def add():
+
+
     new_todo=request.form['new_todo']
-    return new_todo
    
+    my_todos.append(new_todo)
+    #ADD TO LIST
+    #REDIRECT BACK TO INDEX
+    return new_todo
+
 
     
 
